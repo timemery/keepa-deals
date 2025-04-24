c0be2a229eca94e280df0c0a0d7a2371f78c3d5d
timemery <tim@visiblemedia.com>
2025-04-23 20:42:14 -0400

    Restored from a local backup
    This backup was made right before starting work on the Modular version.

--- testfields.py
+++
@@ -1,7 +0,0 @@
-import json
-with open('/home/timscripts/keepa_api/keepa-deals/field_mapping.json') as file:
-    try:
-        json.load(file)
-        print("JSON is valid")
-    except json.JSONDecodeError as e:
-        print(f"JSON error: {e}")
