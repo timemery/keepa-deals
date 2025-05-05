9c416f2d40c1a4ddd42c7680efcf76eb24d1c18b
timemery <tim@visiblemedia.com>
2025-05-05 17:25:00 -0400

    compare updates may 5 5:24

--- header_map.py
+++ header_map.py
@@ -1,6 +1,6 @@
 # header_map.py
 # Critical: Do not remove or modify FUNCTION_MAP without updating headers.json and stable.py
-from stable import get_stat_value, get_title, get_asin, sales_rank_current, used_current, sales_rank_30_days_avg, sales_rank_90_days_avg, sales_rank_180_days_avg, sales_rank_365_days_avg, package_quantity, package_weight, package_height, package_length, package_width, used_like_new, used_very_good, used_good, used_acceptable, new_3rd_party_fbm_current, list_price, used_like_new as stable_used_like_new, used_very_good as stable_used_very_good, used_good as stable_used_good, used_acceptable as stable_used_acceptable, list_price as stable_list_price
+from stable import get_stat_value, get_title, get_asin, sales_rank_current, used_current, sales_rank_30_days_avg, sales_rank_90_days_avg, sales_rank_180_days_avg, sales_rank_365_days_avg, package_quantity, package_weight, package_height, package_length, package_width, used_like_new, used_very_good, used_good, used_acceptable, new_3rd_party_fbm_current, list_price, used_offer_count_current, used_like_new as stable_used_like_new, used_very_good as stable_used_very_good, used_good as stable_used_good, used_acceptable as stable_used_acceptable, list_price as stable_list_price
 
 # Function mapping for headers (in headers.json order)
 FUNCTION_MAP = {
@@ -214,7 +214,7 @@ FUNCTION_MAP = {
     "New Offer Count - 90 days avg.": None,
     "New Offer Count - 180 days avg.": None,
     "New Offer Count - 365 days avg.": None,
-    "Used Offer Count - Current": None,
+    "Used Offer Count - Current": used_offer_count_current,    
     "Used Offer Count - 30 days avg.": None,
     "Used Offer Count - 60 days avg.": None,
     "Used Offer Count - 90 days avg.": None,
